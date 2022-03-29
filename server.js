8943e4bc3e491baee602115fa071a95afd54ed7e
ingthorsson <ingthorsson@gmail.com>
2022-03-28 20:02:41 -0500

    first

---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+const posts = [
+    {
+        username: 'bob',
+        title: 'post1'
+    },
+    {
+        username: 'sam',
+        title: 'post2'
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    //console.log(req.body)
+    res.json(posts.filter(post => post.username === req.user.name));
+});
+
+// Authenticate get the token
+function authenticateToken(req,res,next) {
+    const authHeader = req.headers['authorization']
+    console.log(authHeader)
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user;
+        next();
+    });
+}
+
+
+app.listen(3000);
+
